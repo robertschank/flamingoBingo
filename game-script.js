ae6b632c67f9c85982dd8adf72c1699eb1ccf157
bobbyschank <robertschank@gmail.com>
2017-03-27 20:06:49 -0600

    select squares and game logic

--- game-script.js
+++ game-script.js
@@ -1,33 +1,79 @@
 console.log("game time");
 
+//square constructor
+function Square() {
+	this.marked = false;
+	this.value = value;
+}
 // grab each square in the table
 var squares = document.querySelectorAll("td");
-console.log(squares);
 
-// assign each square a random number between 1 and 75, no repeats
+//create random array of ints from min to max
+function randomArray(min, max) { 
 
+	// create sortedArray between min and max
+	let sortedArray = [];
+	for (let i = min; i < (max + 1); i++) {
+		sortedArray.push(i);
+	}
 
+	let randomArray = [];
+	for (let i = 1; i < 76; i++) {
+		// pick an integer from sortedArray at a random index,
+		let randomIndex = Math.floor(Math.random()*(sortedArray.length));
+		let random = sortedArray[randomIndex];
 
-// for each integer either push() to the end of the array,
-// or unshift() to the front, depending on a random number generator.
+		// remove the element so there are no repeats
+		sortedArray.splice(randomIndex, 1);
 
-let sortedArray = [];
-for (let i = 1; i < 76; i++) {
-	sortedArray.push(i);
-}
-console.log(sortedArray);
-let randomArray = [];
-for (let i = 1; i < 76; i++) {
-
-	let randomIndex = Math.floor(Math.random()*(sortedArray.length-1));
-
-// return Math.floor(Math.random() * (max - min)) + min;
-
-	console.log("randomIndex" + randomIndex);
-	let random = sortedArray[randomIndex];
-	sortedArray.splice(randomIndex, 1);
-	randomArray.push(random);
-
+		// add random int to randomArray
+		randomArray.push(random);
+	}
+	return randomArray;
 }
 
-console.log(randomArray);
+let squareValues = randomArray(1,75);
+
+console.log(squareValues);
+// Assign each square their random value
+for (let i = 0; i < 25; i++) {
+	squares[i].textContent = squareValues[i];
+	// assign each square an index
+	squares[i].dataset.index = i;
+}
+
+// create a random array for "calling" the balls
+let ballOrder = randomArray(1,75);
+console.log(ballOrder);
+
+function callBall() {
+	document.getElementById("current-ball").textContent = ballOrder[0];
+
+}
+callBall();
+
+let rowCount = [0,0,0,0];
+let colCount = [0,0,0,0];
+
+squares.forEach(function(square) {
+	square.addEventListener("click", function() {
+		//if (square.textContent === document.getElementById("current-ball").textContent) {
+			console.log(square);
+			let indy = square.dataset.index;
+			console.log(indy);
+			square.style.backgroundColor = "black";
+			rowCount[indy%5]++;
+			colCount[Math.floor(ind/5)];
+			console.log('rowCount: ' + rowCount);
+			console.log('colCount: ' + colCount);
+
+		//}
+	});
+});
+
+let ind = 20;
+console.log(ind%5);
+console.log();
+
+
+
